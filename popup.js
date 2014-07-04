551afb2bde157ace6331b135bc2a68bef8f2bc4b
bearholmes <bearholmes@gmail.com>
2014-07-04 10:51:59 +0900

    0.4 update

--- popup.js
+++ popup.js
@@ -1,39 +1,60 @@
 /*jshint browser: true */
 /*global chrome */
 function transWiki(){
-var input = document.getElementById('inputArea');
-var output = document.getElementById('outputArea');
-var option = document.getElementsByName('optionTh');
-var cbefore = document.getElementById('contentBefore');
-var cAfter = document.getElementById('contentAfter');
-var contBox = document.getElementById('contBox');
+	var input = document.getElementById('inputArea'); 
+	var output = document.getElementById('outputArea');
+	var optRow = document.getElementsByName('optRow');
+	var optCol = document.getElementsByName('optCol');
+	var cbefore = document.getElementById('contentBefore');
+	var cAfter = document.getElementById('contentAfter');
+	var contBox = document.getElementById('contBox');
+	var msg = document.getElementById('msg');
 
-cbefore.className='off';
+	cbefore.className='off';
+	contBox.className='off';
 
-var result = input.value.replace(/\t/g,' | ');
-result = result.replace(/\n/g,' |\n| ');
-result = '| '+ result;
-var lastBar;
-if(option[0].checked == true){
-var nCount = result.indexOf('\n');
-var oneRow = result.substr(0, nCount);
-var	rHead = oneRow.replace(/\|/g,'||');
-lastBar = result.lastIndexOf('|')-nCount;
-var rBody = result.substr(nCount, lastBar);
-output.innerHTML = rHead + rBody;
-}else{
-lastBar = result.lastIndexOf('|');
-var rBody = result.substr(0,lastBar);
-output.innerHTML = rBody;
+	var result = input.value.replace(/\t/g,' | ');
+		result = result.replace(/\n/g,' |\n| ');
+		result = '| '+ result;
+
+	var rHead = '';
+	var rBody = result;
+
+	if(optRow[0].checked == true){
+			var lineBreak = result.indexOf('\n');
+			var fstRow = result.substr(0, lineBreak+1);
+				rHead = fstRow.replace(/\|/g,'||');
+				rBody = result.substr(lineBreak+1);
+			}
+	if(optCol[0].checked == true){
+				rBody = rBody.replace(/^\|+(.*?)\|+/gm, '||$1||');
+			}
+		rBody = rBody.replace(/\|.$/, '');
+	output.innerHTML = rHead + rBody; 
+
+	cAfter.className='on';
+	msg.className='on';
+	output.focus();
 }
-cAfter.className='on';
-contBox.innerHTML = '<span class="succeed"></span>&nbsp;&nbsp;<strong>Complete</strong>&nbsp;&nbsp;-&nbsp;&nbsp;변환완료 :)'
-output.focus();
+function backWiki(){
+	var input = document.getElementById('inputArea'); 
+	var cbefore = document.getElementById('contentBefore');
+	var cAfter = document.getElementById('contentAfter');
+	var contBox = document.getElementById('contBox');
+	var msg = document.getElementById('msg');
+
+	msg.className='off';
+	cAfter.className='off';
+	cbefore.className='on';
+	contBox.className='on';
+	input.value='';
+	input.focus();
 }
 
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', transWiki);
+  document.querySelector('#tran_btn').addEventListener('click', transWiki);
+  document.querySelector('#back_btn').addEventListener('click', backWiki);
 });
