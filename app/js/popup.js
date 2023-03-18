00c80d6b5cd11f92e5e7b764cbf603ecb5ea2869
bearholmes <bearholmes@gmail.com>
2023-03-18 11:28:22 +0900

    메니페스트 3 대응

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,36 +1,37 @@
-/*jshint browser: true */
-/*global chrome */
 function transWiki() {
-    var input = document.getElementById('inputArea');
-    var output = document.getElementById('outputArea');
-    var optRow = document.getElementsByName('optRow');
-    var optCol = document.getElementsByName('optCol');
-    var cbefore = document.getElementById('contentBefore');
-    var cAfter = document.getElementById('contentAfter');
-    var contBox = document.getElementById('contBox');
-    var msg = document.getElementById('msg');
+  console.log('transWiki')
+  const input = document.getElementById('inputArea');
+  const output = document.getElementById('outputArea');
+  const optRow = document.getElementsByName('optRow');
+  const optCol = document.getElementsByName('optCol');
+  const cBefore = document.getElementById('contentBefore');
+  const cAfter = document.getElementById('contentAfter');
+  const contBox = document.getElementById('contBox');
+  const msg = document.getElementById('msg');
 
-    cbefore.className = 'off';
-    contBox.className = 'off';
+  cBefore.className = 'off';
+  contBox.className = 'off';
 
-    var result = input.value.replace(/\t/g, ' | ');
-    result = result.replace(/\n/g, ' |\n| ');
-    result = '| ' + result;
+  let result = input.value.replace(/\t/g, ' | ');
+  result = result.replace(/\n/g, ' |\n| ');
+  result = '| ' + result;
 
-    var rHead = '';
-    var rBody = result;
+  let rHead = '';
+  let rBody = result;
 
-    if (optRow[0].checked == true) {
-        var lineBreak = result.indexOf('\n');
-        var fstRow = result.substr(0, lineBreak + 1);
-        rHead = fstRow.replace(/\|/g, '||');
-        rBody = result.substr(lineBreak + 1);
-    }
-    if (optCol[0].checked == true) {
-        rBody = rBody.replace(/^\|+(.*?)\|+/gm, '||$1||');
-    }
-    rBody = rBody.replace(/\|.$/, '');
-    output.innerHTML = rHead + rBody;
+  if (optRow[0].checked) {
+    const lineBreak = result.indexOf('\n');
+    const fstRow = result.substring(0, lineBreak + 1);
+    rHead = fstRow.replace(/\|/g, '||');
+    rBody = result.substring(lineBreak + 1);
+  }
+
+  if (optCol[0].checked) {
+    rBody = rBody.replace(/^\|+(.*?)\|+/gm, '||$1||');
+  }
+
+  rBody = rBody.replace(/|.$/, '');
+  output.innerHTML = rHead + rBody;
 
     cAfter.className = 'on';
     msg.className = 'on';
@@ -38,21 +39,22 @@ function transWiki() {
 }
 
 function backWiki() {
-    var input = document.getElementById('inputArea');
-    var cbefore = document.getElementById('contentBefore');
-    var cAfter = document.getElementById('contentAfter');
-    var contBox = document.getElementById('contBox');
-    var msg = document.getElementById('msg');
+  console.log('backWiki')
+  const input = document.getElementById('inputArea');
+  const cBefore = document.getElementById('contentBefore');
+  const cAfter = document.getElementById('contentAfter');
+  const contBox = document.getElementById('contBox');
+  const msg = document.getElementById('msg');
 
-    msg.className = 'off';
-    cAfter.className = 'off';
-    cbefore.className = 'on';
-    contBox.className = 'on';
-    input.value = '';
-    input.focus();
+  msg.className = 'off';
+  cAfter.className = 'off';
+  cBefore.className = 'on';
+  contBox.className = 'on';
+  input.value = '';
+  input.focus();
 }
 
-	  document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#tran_btn').addEventListener('click', transWiki);
-    document.querySelector('#back_btn').addEventListener('click', backWiki);
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#tran_btn').addEventListener('click', transWiki);
+  document.querySelector('#back_btn').addEventListener('click', backWiki);
 });
